f99a46e2be10de3daf51912f728db4fc339b407e
pmacau <pmacau@students.cs.ubc.ca>
2023-02-20 23:25:05 -0800

    Added more UI interaction with play() and sub methods.

--- src/main/model/Board.java
+++ src/main/model/Board.java
@@ -2,24 +2,32 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-import static java.lang.Math.sqrt;
 
 public class Board {
     private List<String> boardGeneration;
     private Integer slots;
     private List<String> board;
+    private List<String> pieces;
+    private List<String> side;
 
 
     // Effects: Constructs a board with the dimensions given, for example, the input 8, would produce a
-    // board of 8^2, 64 slots.
+    // board of 8^2, 64 Tiles.
     // Requires: dimensions > 0, and must be a perfect square.
-    public Board(Integer slots) {
-        boardInteraction(slots);
-    }
-
-    public void boardInteraction(Integer dimensions) {
-        slots = (int) Math.pow(dimensions, 2);
+    public Board(Integer numTiles) {
+        pieces = new ArrayList<>();
+        pieces.add("P"); // pawn
+        pieces.add("B"); // bishop
+        pieces.add("R"); // rook
+        pieces.add("N"); // knight, N instead of Kn for chess convention, and convenience for 1 letter.
+        pieces.add("Q"); // queen
+        pieces.add("K"); // king
+        side = new ArrayList<>();
+        side.add("w"); // white
+        side.add("b"); // black
+        slots = (int) Math.pow(numTiles, 2);
     }
 
 
@@ -27,25 +35,44 @@ public class Board {
         return slots;
     }
 
-    public List<String> rowsAndColumns() {
+    public void genRowsAndColumns() {
         board = new ArrayList();
         for (int i = 0; i < this.getSlots(); i++) {
             board.add("X");
         }
-        return board;
+    }
 
+    public void genBoard() {
+        genRowsAndColumns();
+        Random random = new Random();
+        Integer x;
+        Integer y;
+        Integer indexPiece;
+        Integer indexCord;
+        Integer indexTeam;
+        String team;
+        String piece;
+        x = random.nextInt((int) Math.sqrt(slots));
+        y = random.nextInt((int) Math.sqrt(slots));
+        indexPiece = random.nextInt((pieces.size()));
+        piece = pieces.get(indexPiece);
+        indexTeam = random.nextInt((side.size()));
+        team = side.get(indexTeam);
+        indexCord = y * (int) Math.sqrt(slots) + x;
+        board.set(indexCord, team + "." + piece);
+        getBoard();
     }
 
 
-    //Effects: Generates the board with the given slots
-    //Requires: slots > 0
+    //Effects: Generates the board with the given Tiles
+    //Requires: Tiles > 0
     //Modifies: This
     public List<String> getBoard() {
-        return null;
+        return board;
     }
 
-    public static void displayBoard(List<String> board) {
-        System.out.println(board.toString());
+    public Boolean check(List<String> cords) {
+        return true;
     }
 
 
--- src/main/ui/BoardApp.java
+++ src/main/ui/BoardApp.java
@@ -2,9 +2,10 @@ package ui;
 
 import model.Board;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class BoardApp {
     private Board board;
@@ -39,30 +40,52 @@ public class BoardApp {
 
 
     public void playOptions() {
-        System.out.println("What dimensions would you like the board to be? "
-                +
-                "\n (easy would be 4x4, medium would be 6x6, and hard"
-                +
-                " would be 8x8"
-                + "\n Must be a perfect square e.g 4, 8, 16, 64..."
-                + "\n Type in a perfect square to start.");
+        System.out.println("What dimensions would you like the board to be? " + "\n (easy would be 4x4, medium would be 6x6, and hard" + " would be 8x8" + "\n Must be a perfect square e.g 4, 8, 16, 64..." + "\n Type in a perfect square to start.");
+        init();
+        play();
+    }
+
+    public void init() {
         scan = new Scanner(System.in);
         String dimensionChoice = scan.next();
         Integer numDimension = Integer.parseInt(dimensionChoice);
         board = new Board(numDimension);
-        List<String> listBoard = board.rowsAndColumns();
-        displayRowsAndColumns(listBoard);
+        board.genBoard();
+
+    }
+
+    public void play() {
+        Boolean runningBoard;
+        runningBoard = true;
+        while (runningBoard) {
+            List<String> listBoard = board.getBoard();
+            displayRowsAndColumns(listBoard);
+            System.out.print("\n---------------" + "\n---------------" + "\n---------------");
+            System.out.print("Please recall the positions on the board, separate multiple pieces with a space, also make sure"
+                    + "to use the notation presented in the visual printed, e.g. b.Nx6y5 would indicate a knight on "
+                    + "the 5th row and 6th column presented in the board");
+            scan = new Scanner(System.in);
+            String recall = scan.next();
+            gatherRecalls(recall);
+            if (!board.check(gatherRecalls())) {
+                runningBoard = false;
+            }
+        }
+    }
+
+    public List<String> gatherRecalls(String recall) {
+        return null;
     }
 
     public void displayRowsAndColumns(List<String> board) {
         int rowLength = (int) Math.sqrt(board.size());
         int i = 0;
-            for (String p : board) {
-              System.out.print(p);
+        for (String p : board) {
+            System.out.print(p + " ");
             i++;
             if (i % rowLength == 0) {
                 System.out.println();
-             }
+            }
 
 
         }
